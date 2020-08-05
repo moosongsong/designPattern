2cc55dd9e450bc333f598eb84c82fbb38cc73092
moosongsong <songe08@gmail.com>
2020-08-05 11:24:36 +0900

    refactor: facade 패턴 인코딩 변경

---
+++ Facade Pattern/src/Beverage.java
@@ -0,0 +1,47 @@
+package src;
+
+public abstract class Beverage {
+	protected String name;
+	protected int price;
+	
+	public Beverage(String name, int price) {
+		this.name=name;
+		this.price=price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	public void creator(boolean condiment) { //요게 facade 이다.
+		boilWater();
+		brew();
+		pourInCup();
+		if(condiment) {
+			addCondiment();
+		}
+		
+	}
+
+	public void boilWater() {
+		System.out.println(name+"(을)를 위한 물끓이기");
+	}
+	public void pourInCup() {
+		System.out.println(name+"(을)를 컵에 따릅니다.");
+	}
+	
+	public abstract void brew();
+	public abstract void addCondiment();
+}
---
+++ Facade Pattern/src/Coffee.java
@@ -0,0 +1,38 @@
+package src;
+
+public class Coffee extends Beverage{
+	
+	//
+	private String subName = "에스프레소";
+	//
+	
+	private static String productName = "커피";
+	private static int productPrice=1500;
+	
+	public Coffee() {
+		super(productName, productPrice);
+	}
+
+	public static void setProductName(String productName) {
+		Coffee.productName = productName;
+	}
+
+	public static void setProductPrice(int productPrice) {
+		Coffee.productPrice = productPrice;
+	}
+
+	@Override
+	public void brew() {
+		System.out.println(name+"(을)를 내립니다.");	
+	}
+
+	@Override
+	public void addCondiment() {
+		System.out.println(name+"에 설탕을 첨가합니다.");	
+		func();
+	}
+	
+	public void func() {
+		System.out.println(subName+"완성");
+	}
+}
